193c49df59117380ab10d9f42be3220fcb15d2af
thedudeman69 <christopher.jp.newell@gmail.com>
2021-07-10 17:54:11 -0600

    simple stying changes

--- app.js
+++ app.js
@@ -20,12 +20,12 @@ let autoUpgrades = {
   employee: {
     price: 100,
     quantity: 0,
-    multiplier: 3
+    multiplier: 10
   },
   crew: {
     price: 500,
     quantity: 0,
-    multiplier: 7
+    multiplier: 25
   }
 }
 
@@ -34,8 +34,8 @@ let autoUpgrades = {
 function buyShovel(){
 if (cash >= clickUpgrades.shovel.price){ 
   cash = cash - clickUpgrades.shovel.price,
-  alert('bought'),
   clickUpgrades.shovel.quantity+=1,
+  clickUpgrades.shovel.price+=1,
   cashPC = cashPC + clickUpgrades.shovel.multiplier
 } else {
   alert('not enough cash')
@@ -46,8 +46,8 @@ update()
 function buybigShovel(){
   if (cash >= clickUpgrades.bigShovel.price){ 
     cash = cash - clickUpgrades.bigShovel.price,
-    alert('bought'),
     clickUpgrades.bigShovel.quantity+=1,
+    clickUpgrades.bigShovel.price+=5,
     cashPC = cashPC + clickUpgrades.bigShovel.multiplier
   } else {
     alert('not enough cash')
@@ -57,9 +57,9 @@ function buybigShovel(){
 
 function buyEmployee(){
   if (cash >= autoUpgrades.employee.price){
-    alert('bought'),
     cash = cash - autoUpgrades.employee.price,
     autoUpgrades.employee.quantity+=1,
+    autoUpgrades.employee.price+=20,
     cashPI = cashPI + autoUpgrades.employee.multiplier
   } else {
     alert('not enough cash')
@@ -69,10 +69,10 @@ function buyEmployee(){
 }
 
 function buyCrew(){
-    if (cash >= autoUpgrades.crew.price){ 
-    alert('bought'),
+    if (cash >= autoUpgrades.crew.price){
     cash = cash - autoUpgrades.crew.price,
     autoUpgrades.crew.quantity+=1,
+    autoUpgrades.crew.price+=100,
     cashPI = cashPI + autoUpgrades.crew.multiplier
   } else {
     alert('not enough cash')
@@ -110,6 +110,10 @@ function update() {
   document.getElementById('cashPC').innerText = cashPC
   document.getElementById('cashPI').innerText = cashPI
   document.getElementById('cash').innerText = cash
+  document.getElementById('clickUpgrades.shovel.price').innerText = clickUpgrades.shovel.price
+  document.getElementById('clickUpgrades.bigShovel.price').innerText = clickUpgrades.bigShovel.price
+  document.getElementById('autoUpgrades.employee.price').innerText = autoUpgrades.employee.price
+  document.getElementById('autoUpgrades.crew.price').innerText = autoUpgrades.crew.price
 }
 
 
